a12b350a6f055fdca85e0544bfc588f5bdece76f
Anthonygg7 <70087074+Anthonygg7@users.noreply.github.com>
2020-12-26 22:55:52 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
 
-    apiKey: "AIzaSyAiApJua6GSOU6Jl-jUojKRY8WCnpijDNk",
-    authDomain: "clone-d47f1.firebaseapp.com",
-    databaseURL: "https://clone-d47f1.firebaseio.com",
-    projectId: "clone-d47f1",
-    storageBucket: "clone-d47f1.appspot.com",
-    messagingSenderId: "735843619462",
-    appId: "1:735843619462:web:54854b476219f238b63707",
-    measurementId: "G-2ZGY20G0C5",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 });
 
 const auth = firebase.auth();
